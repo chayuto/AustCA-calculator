286d37b49595c358d7cbd1a36559014b3aaa68a1
Chayut Orapinpatipat <chayut_o@hotmail.com>
2022-09-26 22:06:07 +1000

    convert to kotlin

--- app/src/main/java/me/chayut/austcacal/MainActivity.kt
+++ app/src/main/java/me/chayut/austcacal/MainActivity.kt
@@ -1,102 +1,96 @@
-package me.chayut.austcacal;
+package me.chayut.austcacal
 
-import androidx.appcompat.app.AppCompatActivity;
-import android.content.Context;
-import android.content.SharedPreferences;
-import android.os.Bundle;
-import android.view.View;
-import android.view.inputmethod.InputMethodManager;
-import android.widget.EditText;
-import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.TextView
+import android.widget.EditText
+import android.content.SharedPreferences
+import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import java.lang.Exception
 
-public class MainActivity extends AppCompatActivity {
-
-    private TextView textViewQuotePrice;
-    private EditText editTextFx, editTextPrice, editTextWeight, editTextShippingFee;
-    SharedPreferences prefs;
-
-    public static final String PREFS = "PREFS";
-    public static final String PREF_SHIPPING = "PREF_SHIPPING";
-    public static final String PREF_FX = "PREF_FX";
-    public static final String PREF_PRICE = "PREF_PRICE";
-    public static final String PREF_WEIGHT = "PREF_WEIGHT";
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        textViewQuotePrice = findViewById(R.id.tv_quote_price);
-        editTextFx = findViewById(R.id.et_fx);
-        editTextPrice = findViewById(R.id.et_price);
-        editTextWeight = findViewById(R.id.et_weight);
-        editTextShippingFee = findViewById(R.id.et_shipping);
-
-        prefs = getSharedPreferences(PREFS, MODE_PRIVATE);
-
-        editTextFx.setText(prefs.getString(PREF_FX, "22.5"));
-        editTextPrice.setText(prefs.getString(PREF_PRICE, ""));
-        editTextWeight.setText(prefs.getString(PREF_WEIGHT, ""));
-        editTextShippingFee.setText(prefs.getString(PREF_SHIPPING, "26"));
+class MainActivity : AppCompatActivity() {
+    private var textViewQuotePrice: TextView? = null
+    private var editTextFx: EditText? = null
+    private var editTextPrice: EditText? = null
+    private var editTextWeight: EditText? = null
+    private var editTextShippingFee: EditText? = null
+    private var prefs: SharedPreferences? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        textViewQuotePrice = findViewById(R.id.tv_quote_price)
+        editTextFx = findViewById(R.id.et_fx)
+        editTextPrice = findViewById(R.id.et_price)
+        editTextWeight = findViewById(R.id.et_weight)
+        editTextShippingFee = findViewById(R.id.et_shipping)
+        prefs = getSharedPreferences(PREFS, MODE_PRIVATE)
+        editTextFx!!.setText(prefs!!.getString(PREF_FX, "22.5"))
+        editTextPrice!!.setText(prefs!!.getString(PREF_PRICE, ""))
+        editTextWeight!!.setText(prefs!!.getString(PREF_WEIGHT, ""))
+        editTextShippingFee!!.setText(prefs!!.getString(PREF_SHIPPING, "26"))
     }
 
-    private void saveTextDisplay() {
-        SharedPreferences.Editor editor = prefs.edit();  //Create a SharedPreferences editor
-        editor.putString(PREF_FX, editTextFx.getText().toString());
-        editor.putString(PREF_PRICE, editTextPrice.getText().toString());
-        editor.putString(PREF_WEIGHT, editTextWeight.getText().toString());
-        editor.putString(PREF_SHIPPING, editTextShippingFee.getText().toString());
-        editor.apply();
+    private fun saveTextDisplay() {
+        val editor = prefs!!.edit() //Create a SharedPreferences editor
+        editor.putString(PREF_FX, editTextFx!!.text.toString())
+        editor.putString(PREF_PRICE, editTextPrice!!.text.toString())
+        editor.putString(PREF_WEIGHT, editTextWeight!!.text.toString())
+        editor.putString(PREF_SHIPPING, editTextShippingFee!!.text.toString())
+        editor.apply()
     }
 
-    private void calculate() {
-        float fx, price, weight, shipping;
-        float quote;
-
+    private fun calculate() {
+        val fx: Float
+        val price: Float
+        val weight: Float
+        val shipping: Float
+        val quote: Float
         try {
-            fx = Float.parseFloat(editTextFx.getText().toString());
-            price = Float.parseFloat(editTextPrice.getText().toString());
-            weight = Float.parseFloat(editTextWeight.getText().toString());
-            shipping = Float.parseFloat(editTextShippingFee.getText().toString());
-
-            quote = price * 1.2f * fx + weight * fx * shipping;
-
-            textViewQuotePrice.setText(String.format("%.0f", quote));
-        } catch (Exception ex) {
-            return;
+            fx = editTextFx!!.text.toString().toFloat()
+            price = editTextPrice!!.text.toString().toFloat()
+            weight = editTextWeight!!.text.toString().toFloat()
+            shipping = editTextShippingFee!!.text.toString().toFloat()
+            quote = price * 1.2f * fx + weight * fx * shipping
+            textViewQuotePrice!!.text = String.format("%.0f", quote)
+        } catch (ex: Exception) {
+            return
         }
     }
 
-
-    public void onClickedCalculate(View v) {
-        saveTextDisplay();
-        calculate();
+    fun onClickedCalculate(v: View?) {
+        saveTextDisplay()
+        calculate()
     }
 
-    public void onClickedClear1(View v) {
-        clearAndSetFocus(editTextFx);
+    fun onClickedClear1(v: View?) {
+        clearAndSetFocus(editTextFx)
     }
 
-
-    public void onClickedClear2(View v) {
-        clearAndSetFocus(editTextPrice);
+    fun onClickedClear2(v: View?) {
+        clearAndSetFocus(editTextPrice)
     }
 
-
-    public void onClickedClear3(View v) {
-        clearAndSetFocus(editTextWeight);
+    fun onClickedClear3(v: View?) {
+        clearAndSetFocus(editTextWeight)
     }
 
-
-    public void onClickedClear4(View v) {
-        clearAndSetFocus(editTextShippingFee);
+    fun onClickedClear4(v: View?) {
+        clearAndSetFocus(editTextShippingFee)
     }
 
-    public void clearAndSetFocus(EditText editText) {
-        editText.setText("");
-        editText.requestFocus();
-        InputMethodManager imm =
-                (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-        imm.showSoftInput(editText, InputMethodManager.SHOW_IMPLICIT);
+    fun clearAndSetFocus(editText: EditText?) {
+        editText!!.setText("")
+        editText.requestFocus()
+        val imm = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.showSoftInput(editText, InputMethodManager.SHOW_IMPLICIT)
+    }
+
+    companion object {
+        const val PREFS = "PREFS"
+        const val PREF_SHIPPING = "PREF_SHIPPING"
+        const val PREF_FX = "PREF_FX"
+        const val PREF_PRICE = "PREF_PRICE"
+        const val PREF_WEIGHT = "PREF_WEIGHT"
     }
 }
